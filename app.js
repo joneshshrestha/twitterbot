a4a0a0979bea6a538aeb5357a2fc05dc59a95c93
Jonesh Shrestha <joneshshrestha69@gmail.com>
2018-12-15 11:02:51 +0545

    twitter API tokens added

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log ('hello world!')
+
+var Twit = require('twit') //import twit to node
+var config = require('./config') //import tokens
+
+var T = new Twit(config)
+  
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'A9hTPBmtJzBMWdfDidcOu4pcZ',
+    consumer_secret:      'o7YReRL8qHkE1ajINaXll7uRyqHvfEX5dsM8E7D7mv0IWJ8Zis',
+    access_token:         '1073798050697629696-uqdOuza9F1DcGPX5yHAM6O8e8mZbot',
+    access_token_secret:  'Psp7WTuUHySAW1sucuj3ZfYUSVSPsQiiGNFhXzOSTRP9G'
+}
