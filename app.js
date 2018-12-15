110433bb742c50ec5c2d4b96339d764f9a25a007
Jonesh Shrestha <joneshshrestha69@gmail.com>
2018-12-15 12:57:29 +0545

    implemented get search/tweets which returns only text

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 console.log ('hello world!')
 
-var Twit = require('twit') //import twit to node
-var config = require('./config') //import tokens
+let Twit = require('twit') //import twit to node
+let config = require('./config') //import tokens
 
-var T = new Twit(config)
-  
+let T = new Twit(config)
+
+let params = {
+    q: 'raninbow',
+    count: 2
+}
+
+T.get ('search/tweets', params, gotData)
+
+function gotData(err, data, response) {
+    let tweets = data.statuses
+    for (let i = 0; i < tweets.length; i++) {
+        console.log (tweets[i].text)
+        
+    }
+}
