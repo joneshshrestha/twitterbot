05b78580056c983ee666e40f411f7a28b62acfe7
Jonesh Shrestha <joneshshrestha69@gmail.com>
2018-12-16 12:53:29 +0545

    post implemented

--- app.js
+++ app.js
@@ -5,17 +5,30 @@ let config = require('./config') //import tokens
 
 let T = new Twit(config)
 
-let params = {
-    q: 'raninbow',
-    count: 2
-}
+// let params = {
+//     q: 'raninbow',
+//     count: 2
+// }
 
-T.get ('search/tweets', params, gotData)
+// T.get ('search/tweets', params, gotData)
 
-function gotData(err, data, response) {
-    let tweets = data.statuses
-    for (let i = 0; i < tweets.length; i++) {
-        console.log (tweets[i].text)
+// function gotData(err, data, response) {
+//     let tweets = data.statuses
+//     for (let i = 0; i < tweets.length; i++) {
+//         console.log (tweets[i].text)
         
-    }
+//     }
+// }
+
+var tweet = {
+    status : '#codingrainbow from node.js'
 }
+
+T.post('statuses/update', tweet, tweeted)
+
+function tweeted(err, data, response) {
+    if (err) {
+        console.log('something went wrong')
+    } else
+        console.log('It worked!')
+  }
