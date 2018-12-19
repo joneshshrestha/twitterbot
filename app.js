c414580866541c828d0b7e8ff42844a314584e7f
Jonesh Shrestha <joneshshrestha69@gmail.com>
2018-12-19 20:25:24 +0545

    edit

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-console.log ('hello world!')
+console.log ('Follow bot starting')
 
 let Twit = require('twit') //import twit to node
 let config = require('./config') //import tokens
-
 let T = new Twit(config)
 
-// let params = {
-//     q: 'raninbow',
-//     count: 2
-// }
+//setting up a user stream 
+let stream = T.stream('user')
+//anytime someone follows me
+stream.on ('tweet', tweetEvent)
 
-// T.get ('search/tweets', params, gotData)
-
-// function gotData(err, data, response) {
-//     let tweets = data.statuses
-//     for (let i = 0; i < tweets.length; i++) {
-//         console.log (tweets[i].text)
-        
-//     }
-// }
-
-var tweet = {
-    status : '#codingrainbow from node.js'
+function tweetEvent (eventMsg) {
+    console.log("follow event!")
+    let name = eventMsg.source.name
+    let screenName = eventMsg.source.screen_name
+    tweetIt('.@' + screenName + ' thank you for following me!' )
 }
 
-T.post('statuses/update', tweet, tweeted)
+function tweetIt(txt) {
 
-function tweeted(err, data, response) {
-    if (err) {
-        console.log('something went wrong')
-    } else
-        console.log('It worked!')
-  }
+
+    let tweet = {
+        status : txt
+    }
+
+    T.post('statuses/update', tweet, tweeted)
+
+    function tweeted(err, data, response) {
+        if (err) {
+            console.log('something went wrong')
+        } else
+            console.log('It worked!')
+    }
+}
+    
